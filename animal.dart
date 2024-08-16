440578aad7556153992ff07966a04bad8ffc5635
basiwmza007 <3651051541152@rmutr.ac.th>
2024-08-16 12:32:33 +0700

    second commit

---
+++ animal.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+class Animal {
+  String? name;
+  int? age;
+
+  Animal(this.name, this.age);
+
+  Animal.fromJson(Map<String, dynamic>json){
+    json.forEach((k, v){
+      name = k ;
+      age = v ;
+    });
+  }
+
+  Animal.fromjsonString(String jsonString){
+    Map<String, dynamic> json = jsonDecode(jsonString);
+    json.forEach((k, v){
+      name = k ;
+      age = v ;
+    });
+  }
+  String toString() => 'Name : $name  Age : $age';
+}
+void main(List<String> args) {
+
+  var dang = Animal('Dang', 5);
+  Animal cat = Animal.fromJson({"Sumo":5, "Lucky":10});
+  var dog = Animal.fromjsonString('{"chang":4, "noi":2}');
+
+  print(dang);
+  print(cat);
+  print(dog);
+
+}
